8c59ac5e29eb5636ba3be15584e8fedbda169d73
januszoles <37184337+januszoles@users.noreply.github.com>
2018-03-08 17:57:51 +0100

    Create show-script-panel.jsx

---
+++ show-script-panel.jsx
@@ -0,0 +1,11 @@
+//~ === Scripts === Window Menu:Utilities === 52224 =======================
+//~ ---- Show "Scripts Panel" ---------------------------------------------
+function showScriptsPanel() {
+    try { 
+      app.panels.itemByName("$ID/Scripts").visible = true;
+//~   app.menuActions.itemByID(52224).invoke(); 
+    } catch(e) {
+        alert(e);
+        writeLog(e);
+      };
+}
